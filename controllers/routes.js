d3aff13acf2ac5c017f18060e3488e3753142b62
Reid Henderson <reid.henderson@gmail.com>
2017-06-17 11:38:17 -0400

    bug fix

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = function (app)  {
   // Main "/" Route. This will redirect the user to our rendered React application
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html");
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // This is the route we will send GET requests to retrieve our most recent click data.
