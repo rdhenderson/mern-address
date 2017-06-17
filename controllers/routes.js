5a842f4d61f0e7617ecf31d15f88dc1713b13dbf
Reid Henderson <reid.henderson@gmail.com>
2017-06-17 11:36:05 -0400

    updated path issue

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,8 +1,9 @@
+const path = require('path');
 
 module.exports = function (app)  {
   // Main "/" Route. This will redirect the user to our rendered React application
   app.get("/", function(req, res) {
-    res.sendFile(__dirname + "../public/index.html");
+    res.sendFile(path.join(__dirname, "../public/index.html");
   });
 
   // This is the route we will send GET requests to retrieve our most recent click data.
